63d0a41087d8e425ba00070c439fb5ce59d4629c
Andrey Novikov <novikov@gmail.com>
2013-02-21 18:49:26 +0300

    Support for Jinja2 extensions

--- src/markdoc/templates.py
+++ src/markdoc/templates.py
@@ -12,17 +12,18 @@ Config.register_default('use-default-templates', True)
 
 def build_template_env(config):
     """Build a Jinja2 template environment for a given config."""
-    
+
     load_path = []
-    
+
     if p.isdir(config.template_dir):
         load_path.append(config.template_dir)
-    
+
     if config['use-default-templates']:
         load_path.append(markdoc.default_template_dir)
-    
-    environment = jinja2.Environment(loader=jinja2.FileSystemLoader(load_path))
+
+    environment = jinja2.Environment(loader=jinja2.FileSystemLoader(load_path), extensions=config.get('jinja.extensions', []))
     environment.globals['config'] = config
+
     return environment
 
 
